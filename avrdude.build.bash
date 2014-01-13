7720662f7bc441855fc476c88580afd76938c43e
Federico Fissore <federico@fissore.org>
2014-01-13 16:58:42 +0100

    Revert "Statically linking avrdude so that older mac versions don't complain"
    This reverts commit e9a3658e8651624182a070516fd2d1c5021c24f7.

--- avrdude.build.bash
+++ avrdude.build.bash
@@ -18,7 +18,7 @@ cd avrdude-build
 CONFARGS=" \
 	--prefix=$PREFIX"
 
-CFLAGS="-w $CFLAGS" CXXFLAGS="-w $CXXFLAGS" LDFLAGS="$LDFLAGS -static" ../avrdude-5.11.1/configure $CONFARGS
+CFLAGS="-w $CFLAGS" CXXFLAGS="-w $CXXFLAGS" ../avrdude-5.11.1/configure $CONFARGS
 
 if [ -z "$MAKE_JOBS" ]; then
 	MAKE_JOBS="2"
