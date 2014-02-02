95f6f7b0839a17e0e05eb49349c7261da2cba0a0
Federico Fissore <federico@fissore.org>
2014-02-02 23:11:52 +0100

    [atmel 3.4.3] avr-libc

--- clean.bash
+++ clean.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-rm -rf gcc-4.8.1 gmp-5.0.2 mpc-0.9 mpfr-3.0.0 binutils-2.23.2 avr-libc-1.6.4 avrdude-5.11.1 gpio-avrdude-1138 *-build
+rm -rf gcc-4.8.1 gmp-5.0.2 mpc-0.9 mpfr-3.0.0 binutils-2.23.2 avr-libc-1.8.0 avr8-headers-6.2.0.142 avrdude-5.11.1 gpio-avrdude-1138 *-build
 
 rm -rf objdir/{info,man,share}
 
