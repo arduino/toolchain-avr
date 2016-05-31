c946b9c81fef6547664127c4f53f0fcd0b28b50b
Cristian Maglie <c.maglie@bug.st>
2016-05-31 16:36:41 +0200

    Simplified build scripts

---
+++ package-avr-gcc.bash
@@ -0,0 +1,85 @@
+#!/bin/bash -ex
+# Copyright (c) 2014-2016 Arduino LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+OUTPUT_VERSION=4.9.2-arduino
+
+export OS=`uname -o || uname`
+export TARGET_OS=$OS
+
+if [[ $CROSS_COMPILE == "mingw" ]] ; then
+
+  export CC="i686-w64-mingw32-gcc"
+  export CXX="i686-w64-mingw32-g++"
+  export CROSS_COMPILE_HOST="i686-w64-mingw32"
+  export TARGET_OS="Windows"
+  OUTPUT_TAG=i686-w64-mingw32
+
+elif [[ $OS == "GNU/Linux" ]] ; then
+
+  export MACHINE=`uname -m`
+  if [[ $MACHINE == "x86_64" ]] ; then
+    OUTPUT_TAG=x86_64-pc-linux-gnu
+  elif [[ $MACHINE == "i686" ]] ; then
+    OUTPUT_TAG=i686-pc-linux-gnu
+  elif [[ $MACHINE == "armv7l" ]] ; then
+    OUTPUT_TAG=armhf-pc-linux-gnu
+  else
+    echo Linux Machine not supported: $MACHINE
+    exit 1
+  fi
+
+elif [[ $OS == "Msys" || $OS == "Cygwin" ]] ; then
+
+  export PATH=$PATH:/c/MinGW/bin/:/c/cygwin/bin/
+  export CC="mingw32-gcc -m32"
+  export CXX="mingw32-g++ -m32"
+  export CFLAGS="-DWIN32 -static"
+  export CXXFLAGS="-DWIN32 -static"
+  export LDFLAGS="-DWIN32 -static"
+  OUTPUT_TAG=i686-mingw32
+
+elif [[ $OS == "Darwin" ]] ; then
+
+  export PATH=/opt/local/libexec/gnubin/:/opt/local/bin:$PATH
+  export CC="gcc -arch i386 -mmacosx-version-min=10.5"
+  export CXX="g++ -arch i386 -mmacosx-version-min=10.5"
+  OUTPUT_TAG=i386-apple-darwin11
+
+else
+
+  echo OS Not supported: $OS
+  exit 2
+
+fi
+
+rm -rf autoconf-2.64 automake-1.11.1
+rm -rf gcc gmp-5.0.2 mpc-0.9 mpfr-3.0.0 binutils libc avr8-headers gdb
+rm -rf toolsdir objdir *-build
+
+./tools.bash
+./binutils.build.bash
+./gcc.build.bash
+./avr-libc.build.bash
+./gdb.build.bash
+
+rm -rf objdir/{info,man,share}
+
+rm -f avr-gcc-${OUTPUT_VERSION}-${OUTPUT_TAG}.tar.bz2
+mv objdir avr
+tar -cjvf avr-gcc-${OUTPUT_VERSION}-${OUTPUT_TAG}.tar.bz2 avr
+mv avr objdir
+
