fd426b829a12a8d2a925385cb26678e82fba6344
Martino Facchin <m.facchin@arduino.cc>
2017-12-20 11:06:03 +0100

    Remove gdb patches

--- gdb.build.bash
+++ gdb.build.bash
@@ -40,13 +40,13 @@ fi
 
 tar xfv avr-gdb.tar.bz2
 
-cd gdb
-for p in ../avr-gdb-patches/*.patch
-do
-	echo Applying $p
-	patch -p1 < $p
-done
-cd -
+#cd gdb
+#for p in ../avr-gdb-patches/*.patch
+#do
+#	echo Applying $p
+#	patch -p1 < $p
+#done
+#cd -
 
 mkdir -p objdir
 cd objdir
